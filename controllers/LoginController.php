7625b8633282a6f44936ca664670900440d33945
Mauricio <maau.cursos@gmail.com>
2026-07-09 16:35:47 -0600

    Creando el Routing y Controlador para autenticacion

---
+++ controllers/LoginController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Controllers;
+
+class LoginController { 
+
+    public static function login(){
+        echo "desde login";
+
+    } 
+    public static function logout(){
+        echo "desde logout";
+
+    } 
+    public static function olvide(){
+        echo "olvide";
+
+    } 
+    public static function recuperar(){
+        echo "recuperar";
+
+    } 
+    public static function crear(){
+        echo "crear cuenta";
+
+    } 
+
+}
