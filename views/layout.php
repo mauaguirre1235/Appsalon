7c88dd3a18c3c5a7edaa11d8a03e60b9338b4522
Mauricio <maau.cursos@gmail.com>
2026-07-13 13:42:30 -0600

    CSS ala vista

--- views/layout.php
+++ views/layout.php
@@ -6,10 +6,16 @@
     <title>App Salón</title>
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;700;900&display=swap" rel="stylesheet"> 
     <link rel="stylesheet" href="build/css/app.css">
-</head>
+</head> 
 <body>
 
-    <?php echo $contenido; ?>
+    <div class="contenedor-app">
+        <div class="imagen"></div>
+        <div class="app">
+ <?php echo $contenido; ?>
+        </div>
+    </div>
+   
             
 </body>
 </html>
