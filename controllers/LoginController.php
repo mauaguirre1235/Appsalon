0f9a2326649d72314d0264f201bc72f5195f43d2
Mauricio <maau.cursos@gmail.com>
2026-07-30 13:39:17 -0600

    Almacenar el nuevo password

--- controllers/LoginController.php
+++ controllers/LoginController.php
@@ -127,6 +127,25 @@ class LoginController
 
         if($_SERVER['REQUEST_METHOD'] == 'POST') {
             // Leer el nuevo password y guardarlo
+
+            $password = new Usuario($_POST);
+           $alertas = $password->validarPassword();
+
+           if(empty($alertas)) {
+
+                $usuario->password = null;
+
+                $usuario->password =$password->password;
+                $usuario->hashPasword(); 
+                $usuario->token = null;
+
+                $resultado = $usuario->guardar();
+                if($resultado) {
+                    header('Location: /');
+                }
+ 
+           }
+            //debuguear($password);
         }
 
         $alertas = Usuario::getAlertas();
--- models/Usuario.php
+++ models/Usuario.php
@@ -92,7 +92,18 @@ class Usuario extends ActiveRecord
        
         return self::$alertas;
     }
-    
+
+    public function validarPassword(){
+        if(!$this->password) {
+            self::$alertas['error'][] = 'El password es obligatorio';
+        } 
+        if(strlen($this->password) < 6 ){
+            self::$alertas['error'][] = 'El password debe tener al menos 6 caracteres';
+        }
+
+        return self::$alertas; 
+    }
+
     public function existeUsuario()
     {
         $query = "SELECT * FROM " . self::$tabla . " WHERE email = '" . $this->email . "' LIMIT 1 ";
