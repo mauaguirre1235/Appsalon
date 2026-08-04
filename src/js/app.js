9d39a7b0abba567b51e99af1f757801d5a4029b6
Mauricio <maau.cursos@gmail.com>
2026-08-04 10:31:12 -0600

    Cambiando la seccion segun el tab presionado

--- src/js/app.js
+++ src/js/app.js
@@ -8,16 +8,28 @@ function iniciarApp() {
     tabs(); // cambiar la seccion cuando se presione los tabs
 }
 
-function mostrarSeccion(){
-    console.log('Mostrando seccion..');
+function mostrarSeccion() {
+
+    // ocultar las seccion que tenga la clase de mostrar
+    const seccionAnterior = document.querySelector('.mostrar');
+    if (seccionAnterior) {
+        seccionAnterior.classList.remove('mostrar');
+    }
+
+    // seleccionar la seccion con el paso..
+    const pasoSelector = `#paso-${paso}`;
+    const seccion = document.querySelector(pasoSelector);
+
+    seccion.classList.add('mostrar');
 }
+
 
 function tabs() {
     const botones = document.querySelectorAll('.tabs button');
 
-    botones.forEach( boton => {
-        boton.addEventListener('click', function(e) {
-            paso = parseInt( e.target.dataset.paso); 
+    botones.forEach(boton => {
+        boton.addEventListener('click', function (e) {
+            paso = parseInt(e.target.dataset.paso);
 
             mostrarSeccion();
         });
