45a608b8915ccad56fb050ca686fccafb9a4114e
Mauricio <maau.cursos@gmail.com>
2026-08-04 10:43:53 -0600

    Finalizando la navegacion de tabs

--- src/js/app.js
+++ src/js/app.js
@@ -5,6 +5,7 @@ document.addEventListener('DOMContentLoaded', function () {
 });
 
 function iniciarApp() {
+    mostrarSeccion(); // Muestra y oculta las secciones
     tabs(); // cambiar la seccion cuando se presione los tabs
 }
 
@@ -19,8 +20,18 @@ function mostrarSeccion() {
     // seleccionar la seccion con el paso..
     const pasoSelector = `#paso-${paso}`;
     const seccion = document.querySelector(pasoSelector);
-
     seccion.classList.add('mostrar');
+
+    // Quita la clase de actual al tab anterior
+    const tabAnteriror = document.querySelector('.actual');
+    if (tabAnteriror) {
+        tabAnteriror.classList.remove('actual');
+    }
+
+    // Resalta el tab actual 
+    const tab = document.querySelector(`[data-paso="${paso}"]`);
+    tab.classList.add('actual');
+
 }
 
 
