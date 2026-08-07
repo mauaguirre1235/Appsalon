94c6cd6124676a1324e0d5352a160083320d0920
Mauricio <maau.cursos@gmail.com>
2026-08-07 13:31:43 -0600

    Cambiar la apariencia de un servicio seleccionado

--- src/js/app.js
+++ src/js/app.js
@@ -143,9 +143,12 @@ function mostrarServicios(servicios){
 }
 
 function seleccionarServicio(servicio){
+    const {id} = servicio;
     const { servicios } = cita;
 
     cita.servicios = [...servicios, servicio];
 
-    console.log(cita);
+    const divServicio = document.querySelector(`[data-id-servicio="${id}"]`);
+    divServicio.classList.add('seleccionado');
+    console.log(servicio);
 }
