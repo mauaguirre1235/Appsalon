8552123362c4a6162c9aa095d814a584c439e534
Mauricio <maau.cursos@gmail.com>
2026-08-10 10:48:34 -0600

    Agregar / quitar servicios

--- src/js/app.js
+++ src/js/app.js
@@ -4,9 +4,9 @@ const pasoFinal = 3;
 
 const cita = {
     nombre = '',
-    fecha = '', 
+    fecha = '',
     hora = '',
-    servicios: [] 
+    servicios: []
 }
 
 document.addEventListener('DOMContentLoaded', function () {
@@ -18,7 +18,7 @@ function iniciarApp() {
     tabs(); // cambiar la seccion cuando se presione los tabs
     botonesPaginador(); // agrega o quita los botones del paginador
     paginaSiguiente();
-    paginaAnterior(); 
+    paginaAnterior();
 
     consultarAPI(); // Consulta la API en el backend en PHP
 }
@@ -79,35 +79,35 @@ function botonesPaginador() {
 
     mostrarSeccion();
 
-} 
+}
 
-function paginaAnterior(){
-    const paginaAnterior = document.querySelector('#anterior'); 
-    paginaAnterior.addEventListener('click', function(){
-       
-        if(paso <= pasoInicial) return;
+function paginaAnterior() {
+    const paginaAnterior = document.querySelector('#anterior');
+    paginaAnterior.addEventListener('click', function () {
+
+        if (paso <= pasoInicial) return;
         paso--;
         botonesPaginador();
     })
 }
 
-function paginaSiguiente(){
- const paginaSiguiente = document.querySelector('#siguiente'); 
-    paginaSiguiente.addEventListener('click', function(){
-       
-        if(paso >= pasoFinal) return;
+function paginaSiguiente() {
+    const paginaSiguiente = document.querySelector('#siguiente');
+    paginaSiguiente.addEventListener('click', function () {
+
+        if (paso >= pasoFinal) return;
         paso++;
         botonesPaginador();
     })
 }
 
-async function consultarAPI(){
+async function consultarAPI() {
     try {
         const url = 'http://localhost:3000/api/Servicios';
         const resultado = await fetch(url);
         const servicios = await resultado.json();
         mostrarServicios(servicios);
-        
+
     } catch (error) {
         console.log(error);
     }
@@ -115,9 +115,9 @@ async function consultarAPI(){
 
 }
 
-function mostrarServicios(servicios){
+function mostrarServicios(servicios) {
     servicios.forEach(servicio => {
-        const {id, nombre, precio } = servicio;
+        const { id, nombre, precio } = servicio;
         const nombreServicio = document.createElement('P');
         nombreServicio.classList.add('nombre-servicio');
         nombreServicio.textContent = nombre;
@@ -129,7 +129,7 @@ function mostrarServicios(servicios){
         const servicioDiv = document.createElement('DIV');
         servicioDiv.classList.add('servicio');
         servicioDiv.dataset.idServicio = id;
-        servicioDiv.onclick = function(){
+        servicioDiv.onclick = function () {
             seleccionarServicio(servicio);
         }
 
@@ -142,13 +142,25 @@ function mostrarServicios(servicios){
     });
 }
 
-function seleccionarServicio(servicio){
-    const {id} = servicio;
+function seleccionarServicio(servicio) {
+    const { id } = servicio;
     const { servicios } = cita;
 
-    cita.servicios = [...servicios, servicio];
 
+    // editificar el elemento al que se le da click
     const divServicio = document.querySelector(`[data-id-servicio="${id}"]`);
-    divServicio.classList.add('seleccionado');
-    console.log(servicio);
+
+    // comprobar si un servicio ya fue agregado 
+    if (servicios.some(agregado => agregado.id === id)) {
+        // Eliminarlo
+        cita.servicios = servicios.filter(agregado => agregado.id !== id);
+         divServicio.classList.remove('seleccionado');
+    } else {
+        // Agregarlo 
+        cita.servicios = [...servicios, servicio];
+         divServicio.classList.add('seleccionado');
+    }
+
+
+    console.log(cita);
 }
