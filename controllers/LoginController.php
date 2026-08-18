e3db27a25f3b382150b43337c2f94ed716533279
Mauricio <maau.cursos@gmail.com>
2026-08-18 11:11:31 -0600

    Añadir un boton para cerrar sesion

--- controllers/LoginController.php
+++ controllers/LoginController.php
@@ -65,7 +65,11 @@ class LoginController
     }
     public static function logout()
     {
-        echo "desde logout";
+        session_start();
+
+        $_SESSION = [];
+
+        header('Location: /');
 
     }
     public static function olvide(Router $router)
--- views/cita/index.php
+++ views/cita/index.php
@@ -3,6 +3,7 @@
 
 <div class="barra">
     <p>Hola: <?php echo $nombre ?? ''; ?> </p>
+    <a class="boton" href="/logout">Cerrar Sesion</a>
 </div>
 <div id="app">  
     <nav class="tabs">
